4bdac1ae63b1bc1080e25b255a8904e015f93cec
pratik-win-PC <pratiktravelogue@gmail.com>
2024-01-04 19:42:55 +0530

    Payment Controllers and Routes added (Modification needed). (.env) file pushed. (.gitignore) updated

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
+config({path:"/.env"});
+
 const app = express()
 app.use(cors({
   origin: process.env.CORS_ORIGIN
@@ -11,5 +13,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+app.use("/api", routes);
+
+app.get("/api/getkey", (req,res) =>
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
+);
 
 export {app}
